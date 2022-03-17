49f2a8f9abafb617e9de09a35225dc8d8fd6e78d
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-03-17 12:15:03 -0300

    erro

--- src/challenges.js
+++ src/challenges.js
@@ -1,78 +1,33 @@
 // Desafio 1
-function compareTrue(p1, p2) {
-  if (p1 && p2 === false) {
-    return false;
-  } if (p1 && p2 === true) {
-    return true;
-  }
-  return false;
+function compareTrue() {
+  // seu código aqui
 }
-
-compareTrue(true, false);
-compareTrue(true, true);
-compareTrue(false, false);
 
 // Desafio 2
-function calcArea(base, height) {
-  //  let base = 10;
-  //  let height = 5;
-  // base*altura/2
-  let areaTriangulo = (base * height) / 2;
-  if (areaTriangulo === 250) {
-    return 250;
-  }
-  if (areaTriangulo === 5) {
-    return 5;
-  } if (areaTriangulo === 25.5) {
-    return 25.5;
-  }
+function calcArea() {
+  // seu código aqui
 }
-
-calcArea(10, 50);
-calcArea(5, 2);
-calcArea(51, 1);
 
 // Desafio 3
-function splitSentence(string) {
-  const array = string.split(' '); // https://www.w3schools.com/jsref/jsref_split.asp
-  return array;
+function splitSentence() {
+  // seu código aqui
 }
-
-splitSentence('go Trybe');
-splitSentence('vamo que vamo');
-splitSentence('foguete');
 
 // Desafio 4
-function concatName(nomes) {
-  let firstName = nomes[0];
-  let lastName = nomes[nomes.length - 1];
-  let result = [lastName, firstName];
-
-  return result;
+function concatName() {
+  // seu código aqui
 }
-
-let n1 = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-let n2 = ['foguete', 'não', 'tem', 'ré'];
-let n3 = ['captain', 'my', 'captain'];
-console.log(concatName(n1));
-console.log(concatName(n2));
-console.log(concatName(n3));
 
 // Desafio 5
-function footballPoints(wins, ties) {
-  let a = wins * (3);
-  let b = ties * (1); // http://www.w3bai.com/pt/js/js_arithmetic.html
-  return ((a) + b);
+function footballPoints() {
+  // seu código aqui
 }
-
-footballPoints(14, 8);
-footballPoints(1, 2);
-footballPoints(0, 0);
 
 // Desafio 6
 function highestCount() {
-
+  // seu código aqui
 }
+
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
