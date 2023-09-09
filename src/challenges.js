35b8bde339d05239b9897748970949e3e5500c5f
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-09-09 16:23:42 -0300

    refactor: refatora funções
    '

--- src/challenges.js
+++ src/challenges.js
@@ -1,149 +1,104 @@
 // Desafio 1
-function compareTrue(p1, p2) {
-  if (p1 && p2 === false) {
-    return false;
-  } if (p1 && p2 === true) {
-    return true;
-  }
-  return false;
+function compareTrue(param1, param2) {
+  return param1 === true && param2 === true;
 }
-
-compareTrue(true, false);
-compareTrue(true, true);
-compareTrue(false, false);
 
 // Desafio 2
 function calcArea(base, height) {
-  //  let base = 10;
-  //  let height = 5;
-  // base*altura/2
-  let areaTriangulo = (base * height) / 2;
-  if (areaTriangulo === 250) {
-    return 250;
-  }
-  if (areaTriangulo === 5) {
-    return 5;
-  } if (areaTriangulo === 25.5) {
-    return 25.5;
-  }
+  return (base * height) / 2;
 }
-
-calcArea(10, 50);
-calcArea(5, 2);
-calcArea(51, 1);
 
 // Desafio 3
 function splitSentence(string) {
-  const array = string.split(' '); // https://www.w3schools.com/jsref/jsref_split.asp
-  return array;
+  return string.split(' ');
 }
-
-splitSentence('go Trybe');
-splitSentence('vamo que vamo');
-splitSentence('foguete');
 
 // Desafio 4
 function concatName(nomes) {
-  let firstName = nomes[0];
-  let lastName = nomes[nomes.length - 1];
-  let result = `${lastName}, ${firstName}`;
+  const lastName = nomes[nomes.length - 1];
 
-  return result;
+  return `${lastName}, ${nomes[0]}`;
 }
-
-let n1 = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-let n2 = ['foguete', 'não', 'tem', 'ré'];
-let n3 = ['captain', 'my', 'captain'];
-console.log(concatName(n1));
-console.log(concatName(n2));
-console.log(concatName(n3));
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let a = wins * (3);
-  let b = ties * (1); // http://www.w3bai.com/pt/js/js_arithmetic.html
-  return ((a) + b);
+  return wins * 3 + ties;
 }
-
-footballPoints(14, 8);
-footballPoints(1, 2);
-footballPoints(0, 0);
 
 // Desafio 6
-function highestCount(o) {
-  let maiorNumero = Math.max.apply(null, o);
-  // console.log(maiorNumero);
-  let contador = 0;
-  for (let i = 0; i < o.length; i += 1) {
-    if (maiorNumero === o[i]) {
-      contador += 1;
-    }
-  }
-  return contador;
+function highestCount(numbers) {
+  const maiorNumero = Math.max(...numbers);
+
+  const count = numbers.reduce(
+    (acc, number) => (number === maiorNumero ? acc + 1 : acc),
+    0,
+  );
+
+  return count;
 }
-
-let a1 = [9, 1, 2, 3, 9, 5, 7];
-let a2 = [0, 4, 4, 4, 9, 2, 1];
-let a3 = [0, 0, 0];
-
-highestCount(a1);
-highestCount(a2);
-highestCount(a3);
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let dis1 = Math.abs(cat1 - mouse); // 0 - 1 = -1
-  let dis2 = Math.abs(cat2 - mouse); // 2 - 1 = 1
-  // console.log(dis1, dis2);
-  if (dis1 < dis2) {
-    return 'cat1';
-  }
-  if (dis1 > dis2) {
-    return 'cat2';
-  }
+  const dis1 = Math.abs(cat1 - mouse);
+  const dis2 = Math.abs(cat2 - mouse);
+
+  if (dis1 < dis2) return 'cat1';
+
+  if (dis1 > dis2) return 'cat2';
+
   return 'os gatos trombam e o rato foge';
 }
 
-console.log(catAndMouse(0, 5, 10));
-console.log(catAndMouse(0, 10, 5));
-console.log(catAndMouse(0, 10, 10));
-
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayNumbers) {
+  return arrayNumbers.map((num) => {
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzBuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+
+    return 'bug!';
+  });
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  const object = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  const replace = string.replace(/[aeiou]/gi, (match) => object[match]);
+
+  return replace;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  const object = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  const replace = string.replace(/[12345]/gi, (match) => object[match]);
+
+  return replace;
 }
 
 // Desafio 10
-function techList(ntech, no) {
-  ntech = ntech.sort();
-  let arrayTech = [{
-    tech: ntech[0],
-    name: no,
-  }];
-  if (ntech.length === 0) {
-    return 'Vazio!';
-  }
-  for (let i = 1; i < ntech.length; i += 1) {
-    let novoValor = {
-      tech: ntech[i],
-      name: no,
-    };
-    arrayTech.push(novoValor);
-  }
-  return arrayTech.sort();
-}
+function techList(tech, nome) {
+  if (tech.length === 0) return 'Vazio!';
 
-let array1 = ['React', 'Python', 'CSS', 'JavaScript', 'HTML'];
-let nome = 'Rodrigo';
-console.log(techList(array1, nome));
+  const sortedTechName = tech.sort();
+
+  const arrayTech = sortedTechName.map((techName) => ({
+    tech: techName,
+    name: nome,
+  }));
+
+  return arrayTech;
+}
 
 module.exports = {
   calcArea,
